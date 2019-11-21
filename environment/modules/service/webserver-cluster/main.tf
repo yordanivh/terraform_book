0a856645af901acd412f04071863a8c41650abf3
YordanIH <yordan@hashicorp.com>
2019-11-21 15:57:19 +0200

    Updated configuration

--- environment/modules/service/webserver-cluster/main.tf
+++ environment/modules/service/webserver-cluster/main.tf
@@ -17,7 +17,7 @@ data "terraform_remote_state" "db" {
 }
 
 data "template_file" "user_data" {
-  template = file("user-data.sh")
+  template = file("${path.module}/user-data.sh")
 
   vars = {
     server_port = var.server_port
@@ -41,8 +41,7 @@ resource "aws_launch_configuration" "example" {
   image_id        = "ami-0d5d9d301c853a04a"
   instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
-
-  #user_data = data.template_file.user_data.rendered
+  user_data       = data.template_file.user_data.rendered
 
   lifecycle {
     create_before_destroy = true
@@ -72,29 +71,34 @@ resource "aws_security_group" "instance" {
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 }
 #security group for ALB
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
+}
 
+resource "aws_security_group_rule" "allow_http_inbound" {
   #Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port         = local.http_port
+  to_port           = local.http_port
+  protocol          = local.tcp_protocol
+  cidr_blocks       = local.all_ips
+}
 
+resource "aws_security_group_rule" "allow_all_outbound" {
   #Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port         = local.any_port
+  to_port           = local.any_port
+  protocol          = local.any_protocol
+  cidr_blocks       = local.all_ips
+
 }
 
 #Listener rule for ALB
@@ -125,7 +129,7 @@ resource "aws_lb" "example" {
 #defining listener
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   #By default, return a simple 404 page
@@ -155,4 +159,12 @@ resource "aws_lb_target_group" "asg" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+}
+
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
 }

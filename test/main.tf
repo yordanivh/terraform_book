75e754abe54a6ae35719eee5ee66944ac69b95f4
YordanIH <yordan@hashicorp.com>
2019-12-02 15:20:54 +0200

    making changes

--- test/main.tf
+++ test/main.tf
@@ -46,4 +46,15 @@ ${name}
     %{~ endfor ~}
     EOF
 }
- 
+
+
+#if else directive
+
+variable "name" {
+  description = "A name to render"
+  type =string
+}
+
+output "if_else_directive" {
+  value = "Hello, %{ if var.name !="" }${var.name}%{ else } (unnamed)%{ endif }"
+}
